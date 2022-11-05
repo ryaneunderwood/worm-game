e1f582763c8055cacf7ae44e17f8e0a1081560e2
orangeoceans <90552466+orangeoceans@users.noreply.github.com>
2022-11-05 16:40:34 -0700

    Add valid word checking

--- js/ui/game.js
+++ js/ui/game.js
@@ -2,8 +2,8 @@ class Game extends Phaser.Scene {
 
     constructor() {
 	super('game');
-	this.initial_word;
-	this.end_word;
+	this.current_word;
+	this.goal_word;
 	this.past_word;
 	
 	this.input_box;
@@ -15,32 +15,43 @@ class Game extends Phaser.Scene {
 	}
 
     create() {
-	this.initial_word = this.add.text(WINDOW_WIDTH*1/10,WINDOW_HEIGHT*4/10,"START");
-	this.end_word = this.add.text(WINDOW_WIDTH*7/10,WINDOW_HEIGHT*4/10,"END");
-	this.past_word = this.add.text(WINDOW_WIDTH*2/10,WINDOW_HEIGHT*2/10,"", { wordWrap: { width: 400 , useAdvancedWrap: true}});
-	this.input_box = this.add.dom(WINDOW_WIDTH*5/10, WINDOW_HEIGHT*6/10).createFromCache("form");
-	
-	this.return_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-	this.return_key.on('down', function (event) {
-	    let input_word = this.input_box.getChildByName("input_word").value;
-	    if (this.check_word(input_word)) {
-		console.log(input_word);
-	    }
-	    if ( this.counts > 0 ) {
-		this.past_word.setText(this.past_word.text + " -> " + this.initial_word.text);
-	    }
-	    else { this.past_word.setText(this.initial_word.text)}
-	    this.initial_word.setText(input_word);
-	    this.counts++;
-	}, this);
+		this.current_word = this.add.text(WINDOW_WIDTH*1/10,WINDOW_HEIGHT*4/10,"START");
+		this.goal_word = this.add.text(WINDOW_WIDTH*7/10,WINDOW_HEIGHT*4/10,"END");
+		this.past_word = this.add.text(WINDOW_WIDTH*2/10,WINDOW_HEIGHT*2/10,"", { wordWrap: { width: 400 , useAdvancedWrap: true}});
+		this.past_word.setText(this.current_word.text);
+		this.input_box = this.add.dom(WINDOW_WIDTH*5/10, WINDOW_HEIGHT*6/10).createFromCache("form");
+		
+		this.return_key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+		this.return_key.on('down', function (event) {
+			let input_word = this.input_box.getChildByName("input_word").value;
+			if (this.check_word(input_word)) {
+				console.log("Good! " + input_word)
+				this.past_word.setText(this.past_word.text + " -> " + input_word);
+				this.current_word.setText(input_word);
+				this.counts++;
+			}
+		}, this);
     }
     
-    check_word(word) {
-	if(word == "") {
-	    return false;
-	}
-	return true;
-    }	
+	check_word(input_word) {
+		let current_word = this.current_word.text;
+		if(input_word === current_word || Math.abs(input_word.length - current_word.length)>=2) {
+			return false;
+		}
 
-    
+		for (let i = 0; i < input_word.length; i++) {
+			if (i >= current_word.length)
+				break;
+
+			if (input_word[i] !== current_word[i]) {
+				if (input_word.length == current_word.length)
+					return input_word.substring(i+1) === current_word.substring(i+1);
+				else if (input_word.length < current_word.length)
+					return input_word.substring(i) === current_word.substring(i+1);
+				else if (input_word.length > current_word.length)
+					return input_word.substring(i+1) === current_word.substring(i);
+			}
+		}
+		return true;
+	}
 }
