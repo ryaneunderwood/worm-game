b5c759c839c4faddfe07586a34d64a323ab37527
ryaneunderwood <rucranberry@gmail.com>
2022-11-06 11:58:07 -0800

    cleanup

--- utils/makedictmap.py
+++ utils/makedictmap.py
@@ -10,7 +10,7 @@ sortedDict=sorted(L, key=len)
 f=open("./utils/wordmap.txt","w")
 
 for i in range(len(sortedDict))[17823:27000]:
-    #f2.write("#"+str(i+1)+"\n"+j[5:]+"\n")
+    
     tree=ct.onestage(sortedDict[i],1,[sortedDict[i]])
     tree1=list( map(itemgetter(0), tree ))
     
@@ -22,18 +22,3 @@ for i in range(len(sortedDict))[17823:27000]:
         print(round(i/len(sortedDict),3)*100,'%')
     
     
-
-
-
-'''
-print('Test 1: cat-cat')
-#a=ct.check_tree_len('xylophone','cat')
-#print(a)
-print('Test 2: cat-bat')
-a=ct.check_tree_len('cat','bat')
-print(a)
-print('Test 3: clap-shin')
-a=ct.check_tree_len('busy','carpet')
-print(a)
-print(ct.diffby1('busy','carp'),ct.diffby1('cat','cbar'), ct.diffby1('cat','car'))
-'''
