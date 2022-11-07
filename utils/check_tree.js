96dfe60c7e232935723572d3f48f8651f466233e
ryaneunderwood <rucranberry@gmail.com>
2022-11-07 13:39:19 -0800

    some code for checktreelen

--- utils/check_tree.js
+++ utils/check_tree.js
@@ -1,19 +1,14 @@
 let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 //console.log(alphabet);
-let wordmap=s
-let L=LW
-for  (let i = 0; i < LW.length; i++) {
-    L[i]]LW[i].strip('\n');
-}
 let cache = this.cache.text;
-let wordgraph= cache.get('wordgraph');
+let wordgraph= cache.get('word_graph'); //load in word_graph text file from cache
 let LW= cache.get('legal_words').split('\n');
 let sortedDict = sorted(LW, {
   "key": len
 });
 function check_tree_len(word1, word2) {
-    let tree1 = [[sortedDict.indexOf(word1), 0, 0]];
-    let tree2 = [[sortedDict.indexOf(word2), 0, 0]];
+    var tree1 = [[sortedDict.indexOf(word1), 0, 0]];
+    var tree2 = [[sortedDict.indexOf(word2), 0, 0]];
 
     if (word1 === word2) {
 	return 0;
@@ -22,10 +17,20 @@ function check_tree_len(word1, word2) {
     let nsteps1 = 1;
     let nsteps2 = 1;
     let fails = 0;
-    while !(tree1.some(r=> tree2.includes(r))){
-	if (fails === 3) {
+    while  (!(tree1.some(r=> tree2.includes(r)))){
+	if (fails === 2) {
 	    return -1;
 	}
-	if tree1.length.
+	
+	if (tree1.length <= tree2.length) {
+	    let tree1a=tree1
+	    var graphindex = tree1a.map(function(arr) {
+		return [arr[x][0]];
+	    });
+	    for (const [index, element] of graphindex.entries()) {
+    		fulllist=wordgraph[1]
+		for 
+	    } 
+	}
     }
 }
